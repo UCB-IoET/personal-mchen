b655ac31a49671713feb76d370afd65125b7c817
Michael Chen <mc.michaelchen.us@gmail.com>
2015-02-08 16:16:53 -0800

    Time limits on button presses, prevent spamming

--- bopitbutton.lua
+++ bopitbutton.lua
@@ -1,27 +1,35 @@
 require "cord"
 sh = require "stormsh"
-Button = require "Button"
-LED = require "Led"
+Button = require "button"
+LED = require "led"
+shield = require "starter"
 
 ipaddr = storm.os.getipaddr()
 
 port = 227
 count = 0
+time_gap = 3000
 -- create client socket
 csock = storm.net.udpsocket(port,
-                            function(payload, from, port)
-				print("Got response: %s", payload)
-                            end)
+	function(payload, from, port)
+		print("Got response: %s", payload)
+		end)
 
 function sendCommand(axis)
+	local new_time = storm.os.now(storm.os.SHIFT_0)/storm.os.MILLISECOND
+	local old_time = last_command_time
+	if new_time - old_time < time_gap then return nil end
+
+	last_command_time = new_time
 	local msg = string.format("%s", axis)
 	print("sent: ", msg, " count: ",  count)
 	storm.net.sendto(csock, msg, "ff02::1", port)
-	count = count +1 
+	count = count + 1 
+	cord.new(function() shield.Buzz.go(80) cord.await(storm.os.invokeLater, 500*storm.os.MILLISECOND) shield.Buzz.stop() end)
 end
 
-
 function start()
+	last_command_time = storm.os.now(storm.os.SHIFT_0)/storm.os.MILLISECOND
 	b1 = Button:new("D9")
 	b2 = Button:new("D10")
 	b3 = Button:new("D11")
@@ -34,8 +42,9 @@ function start()
 	b3:whenever("FALLING", function()
 		sendCommand("z")
 	end)
+	print("setup complete")
 end
 
+start()
 sh.start()
 cord.enter_loop()
-start()

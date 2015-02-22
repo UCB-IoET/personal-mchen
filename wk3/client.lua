e27a04bc974f2e91ef8dd8d7dab17ba1c6c39861
Michael Chen <mc.michaelchen.us@gmail.com>
2015-02-21 16:19:01 -0800

    Complete basic working client

--- wk3/client.lua
+++ wk3/client.lua
@@ -3,6 +3,9 @@ sh = require "stormsh"
 Button = require "button"
 LCD = require "lcd"
 
+local curr_count = 0
+local num_remote = 0
+
 local set = "set"
 
 function printServicePairs(t)
@@ -24,7 +27,7 @@ function printPairs(t, indent)
 end
 
 local remote_services = {} --[from]: table
-local remote_map = {} -- [counter]: from
+local remote_map = {}
 
 local port = 1525
 local sock = storm.net.udpsocket(port,
@@ -32,18 +35,47 @@ local sock = storm.net.udpsocket(port,
 		local response = storm.mp.unpack(payload)
 		print(string.format("Services from %s", from))
 		printServicePairs(response)
-		for k,v in response do
-			if response[k][s] == "setBool" or response[k][s] == "setLed" then
+		local team_name = nil
+
+		for k,v in pairs(response) do
+			if remote_services[from] == nil and (response[k]["s"] == "setBoolean" or response["s"] == "setLed") then
 				response[k][set] = false
 				response[k]["func_name"] = k
-				if remote_services[from] == nil then -- note: only supports one setBool per remote
-					remote_map[num_remote] = from
-					num_remote = num_remote + 1
-				end
 				remote_services[from] = response[k]
+				remote_map[curr_count] = from
+				curr_count = curr_count + 1
+				print("added "..from)
 			end
+
+			if k == "id" then team_name = v end
 		end
+
+		if remote_services[from] ~= nil then remote_services[from]["team_name"] = team_name end
 	end)
+
+
+function disp_lcd(idx)
+	local v1 = remote_map[idx]
+	if remote_services[remote_map[idx]]["team_name"] ~= nil then v1 = remote_services[remote_map[idx]]["team_name"] end
+	local v2 = remote_services[remote_map[idx]]["func_name"]
+	write_to_lcd(v1, v2)
+end
+
+function lcd_setup()
+    lcd = LCD.new(storm.i2c.EXT, 0x7c, storm.i2c.EXT, 0xc4)
+end
+
+function write_to_lcd(value1, value2)
+    cord.new(function ()
+        lcd.init(2, 1)
+        lcd.writeString(value1)
+        lcd.setBacklight(150, 50, 220)
+        if value2 ~= nil then
+        	lcd.setCursor(1, 0)
+        	lcd.writeString(value2)
+        end
+    end)
+end
 
 function invoke_bool(idx)
 	local service = remote_services[remote_map[idx]]
@@ -51,48 +83,35 @@ function invoke_bool(idx)
 	service[set] = new_bool
 	local msg = storm.mp.pack({service["func_name"], {new_bool}})
 	storm.net.sendto(sock, msg, remote_map[idx], 1526)
-end
-
-function disp_lcd(idx)
-	lcd:clear()
-	lcd:setCursor(0, 0)
-	lcd:writeString(remote_map[idx])
-	lcd:setCursor(1, 0)
-	lcd:writeString(remote_services[remote_map[idx]]["func_name"])
-end
-
-
-function svc_stdout(from_ip, from_port, msg)
-  print (string.format("[STDOUT] (ip=%s, port=%d) %s", from_ip, from_port, msg))
+	print("sent command to "..remote_map[idx])
 end
 
 function start()
 	b1 = Button:new("D9")
 	b2 = Button:new("D10")
 	b3 = Button:new("D11")
-	curr = 0
-	num_remote = 0
 
-	b1:whenever("FALLING", function()
-		invoke_bool(curr)
-	end)
-
-	b2:whenever("FALLING", function()
-		curr = curr + 1
-		if curr > num_remote then curr = 0 end
-		disp_lcd(curr)
-	end)
+	curr_count = 0
 
 	b3:whenever("FALLING", function()
-		curr = curr - 1
-		if curr < 0 then curr = num_remote end
-		disp_lcd(curr)
+		invoke_bool(curr_count)
+	end)
+
+	b2:whenever("FALLING", function()		
+		curr_count = curr_count + 1
+		if curr_count > num_remote then curr_count = 0 end
+		disp_lcd(curr_count)
+	end)
+
+	b1:whenever("FALLING", function()
+		curr_count = curr_count - 1
+		if curr_count < 0 then curr_count = num_remote end
+		disp_lcd(curr_count)
 	end)
 end
 
-lcd = LCD:new(storm.i2c.EXT, 0x7c, storm.i2c.EXT, 0xc4)
 
-cord.new(function() lcd:init(2,1) end)
+lcd_setup()
 start()
-sh.start()
+--sh.start()
 cord.enter_loop()

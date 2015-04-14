dfbb7732a69ee8efee1b3e2bbcd139fef3855a93
Michael Chen <mc.michaelchen.us@gmail.com>
2015-04-13 18:00:26 -0700

    Sync with Smap query

--- ChairTalk/app/src/main/java/com/michaelchen/chairtalk/MainActivity.java
+++ ChairTalk/app/src/main/java/com/michaelchen/chairtalk/MainActivity.java
@@ -21,26 +21,23 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
-import java.util.jar.Pack200;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -49,16 +46,51 @@ public class MainActivity extends ActionBarActivity {
     private SeekBar seekBackFan;
     private SeekBar seekBottomHeat;
     private SeekBar seekBackHeat;
+    protected Map<String, String> uuidToKey;
+    protected Map<String, String> keyToUuid;
 //    private static final String uri = "http://shell.storm.pm:38027";
-    private static final String uri = "http://54.215.11.207:38027";
+//    private static final String uri = "http://54.215.11.207:38027";
+    private static final String uri = "http://54.215.11.207:38001";
+    public static final int refreshPeriod = 5000;
+    private Timer timer;
+    private TimerTask timerTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initSeekbarListeners();
-        initSeekbarPositions();
-        initSwitch();
+        setSeekbarPositions();
+//        initSwitch();
+        initMaps();
         updateLastUpdate();
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                MainActivity.this.querySmapView(null);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MainActivity.this.updateLastPullTime();
+                    }
+                });
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 0, refreshPeriod);
+    }
+
+    private void initMaps() {
+        uuidToKey = new HashMap<String, String>();
+        uuidToKey.put("27e1e889-b749-5cf9-8f90-5cc5f1750ddf", getString(R.string.seek_back_fan));
+        uuidToKey.put("33ecc20c-e636-58eb-863f-142717105075", getString(R.string.seek_back_heat));
+        uuidToKey.put("a99daf41-f3b3-51a7-97bf-48fb3e7bf130", getString(R.string.seek_bottom_heat));
+        uuidToKey.put("b7ef2e98-2e0a-515b-b534-69894fdddf6f", getString(R.string.seek_bottom_fan));
+
+        keyToUuid = new HashMap<String, String>();
+        for(Map.Entry<String, String> entry : uuidToKey.entrySet()){
+            keyToUuid.put(entry.getValue(), entry.getKey());
+        }
+
     }
 
     protected void initSeekbarListeners() {
@@ -76,6 +108,7 @@ public class MainActivity extends ActionBarActivity {
 
             public void onStopTrackingTouch(SeekBar seekBar) {
                 MainActivity.this.updatePref(getString(R.string.seek_back_fan), currentPosition);
+                smapButton(null);
             }
         });
 
@@ -93,6 +126,7 @@ public class MainActivity extends ActionBarActivity {
 
             public void onStopTrackingTouch(SeekBar seekBar) {
                 MainActivity.this.updatePref(getString(R.string.seek_bottom_fan), currentPosition);
+                smapButton(null);
             }
         });
 
@@ -110,6 +144,7 @@ public class MainActivity extends ActionBarActivity {
 
             public void onStopTrackingTouch(SeekBar seekBar) {
                 MainActivity.this.updatePref(getString(R.string.seek_back_heat), currentPosition);
+                smapButton(null);
             }
         });
 
@@ -127,11 +162,12 @@ public class MainActivity extends ActionBarActivity {
 
             public void onStopTrackingTouch(SeekBar seekBar) {
                 MainActivity.this.updatePref(getString(R.string.seek_bottom_heat), currentPosition);
+                smapButton(null);
             }
         });
     }
 
-    protected void initSeekbarPositions() {
+    protected void setSeekbarPositions() {
         SharedPreferences sharedPref = this.getSharedPreferences(
                 getString(R.string.temp_preference_file_key), Context.MODE_PRIVATE);
 
@@ -146,6 +182,13 @@ public class MainActivity extends ActionBarActivity {
         seekBottomHeat.setProgress(bottomHeatPos); 
     }
 
+    protected void updateLastPullTime() {
+        Date time = new Date();
+        TextView t = (TextView) findViewById(R.id.textViewlastPullTime);
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        t.setText("Last Update (Pull): " + df.format(time));
+    }
+
 
     protected void updateLastUpdate() {
         SharedPreferences sharedPref = this.getSharedPreferences(
@@ -155,23 +198,8 @@ public class MainActivity extends ActionBarActivity {
             Date time = new Date(lastUpdateTime);
             TextView t = (TextView) findViewById(R.id.textViewlastUpdateTime);
             DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-            t.setText("Last Update: " + df.format(time));
+            t.setText("Last Update (Push): " + df.format(time));
         }
-    }
-
-    protected void initSwitch() {
-        SharedPreferences sharedPref = this.getSharedPreferences(
-                getString(R.string.temp_preference_file_key), Context.MODE_PRIVATE);
-        boolean inChair = sharedPref.getBoolean(getString(R.string.in_chair_key), false);
-        Switch inChairSwitch = (Switch) findViewById(R.id.switchInChair);
-        inChairSwitch.setChecked(inChair);
-        inChairSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                MainActivity.this.updatePref(getString(R.string.in_chair_key), isChecked);
-            }
-        });
-
     }
 
     @Override
@@ -219,14 +247,43 @@ public class MainActivity extends ActionBarActivity {
         int backHeatPos = sharedPref.getInt(getString(R.string.seek_back_heat), 0);
         int bottomHeatPos = sharedPref.getInt(getString(R.string.seek_bottom_heat), 0);
         boolean inChair = sharedPref.getBoolean(getString(R.string.in_chair_key), false);
-        JSONObject jsonobj = createJsonObject(backFanPos, bottomFanPos, backHeatPos, bottomHeatPos, inChair);
+//        JSONObject jsonobj = createJsonObject(backFanPos, bottomFanPos, backHeatPos, bottomHeatPos, inChair);
+        JSONObject jsonobj = tempCreateJsonObject(backFanPos, bottomFanPos, backHeatPos, bottomHeatPos, inChair);
         new HttpAsyncTask(jsonobj).execute(uri);
         TextView t = (TextView) findViewById(R.id.textViewJson);
         t.setText(jsonobj.toString());
     }
 
+    private JSONObject tempCreateJsonObject(int backFan, int bottomFan, int backHeat, int bottomHeat, boolean inChair) {
+        JSONObject jsonobj = new JSONObject();
+        JSONObject header = new JSONObject();
+        try {
+            header.put("devicemodel", android.os.Build.MODEL); // Device model
+            header.put("deviceVersion", android.os.Build.VERSION.RELEASE); // Device OS version
+            header.put("language", Locale.getDefault().getISO3Language()); // Language
+//            jsonobj.put("header", header);
+
+            jsonobj.put("fans", (backFan + bottomFan)/2);
+//            jsonobj.put("bottomFan", bottomFan);
+            jsonobj.put("heaters", (backHeat + bottomHeat)/2);
+//            jsonobj.put("bottomHeater", bottomHeat);
+            jsonobj.put("macaddr", "12345");
+        } catch (JSONException e) {
+
+        }
+        return jsonobj;
+    }
+
+    private void rescheduleTimer() {
+//        TODO: Fix timer rescheduling
+//        timer.cancel();
+//        timer = new Timer();
+//        timer.scheduleAtFixedRate(timerTask, refreshPeriod, refreshPeriod);
+    }
+
     public void smapButton(View view) {
         sendUpdateSmap();
+        rescheduleTimer();
     }
 
     private JSONObject createJsonObject(int backFan, int bottomFan, int backHeat, int bottomHeat, boolean inChair) {
@@ -280,7 +337,6 @@ public class MainActivity extends ActionBarActivity {
                     }
                 });
 
-                client(jsonobj);
                 return true;
             } catch (Exception e) {
                 Log.d("httpPost", "failed");
@@ -289,7 +345,6 @@ public class MainActivity extends ActionBarActivity {
         }
         // onPostExecute displays the results of the AsyncTask.
         protected void onPostExecute(String result) {
-
         }
     }
 
@@ -305,6 +360,72 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    private class SmapQueryAsyncTask extends AsyncTask<String, Void, Boolean> {
+        private String uuid;
+        public static final String QUERY_LINE = "select data before now where uuid = '%s'";
+        public SmapQueryAsyncTask(String uuid) {
+            super();
+            this.uuid = uuid;
+        }
+        @Override
+        protected Boolean doInBackground(String...urls) {
+            for(String url: urls) {
+                final String uri = url;
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            DefaultHttpClient httpclient = new DefaultHttpClient();
+                            HttpPost httpPostReq = new HttpPost(uri);
+                            StringEntity se = new StringEntity(String.format(QUERY_LINE, uuid));
+
+                            httpPostReq.setEntity(se);
+                            HttpResponse httpResponse = httpclient.execute(httpPostReq);
+                            InputStream inputStream = httpResponse.getEntity().getContent();
+                            final String response = inputStreamToString(inputStream);
+                            Log.d("httpPost", response);
+                            JSONObject jsonResponse = new JSONObject(response.substring(1, response.length() - 1));
+                            JSONArray readings = ((JSONArray) jsonResponse.getJSONArray("Readings")).getJSONArray(0);
+                            String retUuid = jsonResponse.getString("uuid");
+                            int value = readings.getInt(1);
+                            MainActivity.this.updatePref(MainActivity.this.uuidToKey.get(retUuid), value);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+//                            Toast.makeText(getBaseContext(), "Post Result: " + response, Toast.LENGTH_SHORT).show();
+                                    MainActivity.this.setSeekbarPositions();
+                                }
+                            });
+                        } catch (Exception e) {
+                            Log.d("httpPost", "failed");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getBaseContext(), "Post Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
+                t.run();
+            }
+            return true;
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        protected void onPostExecute(String result) {
+        }
+    }
+
+    public void querySmapView(View v) {
+        for(String uuid : uuidToKey.keySet()) {
+            querySmap(uuid);
+        }
+    }
+
+    private void querySmap(String uuid) {
+        new SmapQueryAsyncTask(uuid).execute("http://shell.storm.pm:8079/api/query");
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -318,33 +439,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public static void client(JSONObject jsonObject) throws IOException {
-        DatagramSocket clientSocket = new DatagramSocket(2362);
-        InetAddress IPAddress =  InetAddress.getByName("storm.rocks");
-
-        byte[] sendData = jsonObject.toString().getBytes();
-
-        DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length, IPAddress, 2362);
-        clientSocket.send(sendPacket);
-//        byte[] receiveData = new byte[1024];
-//        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-//        clientSocket.receive(receivePacket);
-//        String modifiedSentence = new String(receivePacket.getData());
-//        Log.d("UDP", modifiedSentence);
-        clientSocket.close();
-
-//        Socket socket = new Socket("192.168.1.102", 2362);
-//        OutputStream out = socket.getOutputStream();
-//        PrintWriter output = new PrintWriter(out);
-//        output.print(jsonObject.toString());
-//        out.flush();
-//        out.close();
-
-        Log.d("UDP", "send ok");
-
-        // }
-
     }
 }
